fa104a4f230e279877c68fd15663efb3886903eb
sam <jialin.hou@deeplang.ai>
2024-04-29 17:00:10 +0800

    feat: components

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,17 @@
-import { Button } from '@repo/ui';
+import { Button, Input } from '@repo/ui';
+import { Mail } from 'lucide-react';
 
 export default function Page() {
   return (
     <main>
-      <Button>Click me!</Button>
+      <Button>
+        <Mail className="mr-2 h-4 w-4" /> Login with Email
+      </Button>
+      <Input type="email" placeholder="Email" />
+      <div className="flex w-full max-w-sm items-center space-x-2">
+        <Input type="email" placeholder="Email" />
+        <Button type="submit">Subscribe</Button>
+      </div>
     </main>
   );
 }
